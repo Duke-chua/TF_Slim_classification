17a43fcdd12cd4482ffb85b37031890d19602662
gulingfengze <gulingfengze@126.com>
2019-06-18 11:05:52 +0800

    modified num_classes

--- My_export_inference_graph.sh
+++ My_export_inference_graph.sh
@@ -1,8 +1,8 @@
 python My_export_inference_graph.py \
   --model_name=inception_v4 \
-  --num_classes=33 \
+  --num_classes=5 \
   --image_height=299 \
   --image_width=299 \
   --batch_size=1 \
   --input_node_names=input \
-  --output_file=/home2/wangqiang/JuNengProjects/Classification/freeze_model/inception_v4_inf_graph.pb
+  --output_file=/xx/Classification/freeze_model/inception_v4_inf_graph.pb
